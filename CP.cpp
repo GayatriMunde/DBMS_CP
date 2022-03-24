a1656f9591447545dcf70d4916baa57e28036953
GayatriMunde <mundegr@gmail.com>
2022-03-24 19:34:32 +0530

    Added describe command

--- CP.cpp
+++ CP.cpp
@@ -1,43 +1,99 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void droptable(vector<string> &querytocons)
-{
-    string schema = "schema.txt";
-    string deleteline;
-    if (querytocons.size() < 2)
-    {
-        cout << "Incorrect Query" << endl;
-        return;
-    }
 
+bool isTablePresent(string tname, string &deleteline){
+    string schema = "schema.txt";
     fstream table;
     table.open("schema.txt", ios::in);
-    string tname = querytocons[2];
     bool tablepresent = false;
-    while (table)
-    {
+    while (table){
         string currentline;
         getline(table, currentline);
         cout.flush();
 
-        if (currentline.substr(0, tname.length()) == tname)
-        {
+        if (currentline.substr(0, tname.length()) == tname){
             tablepresent = true;
             deleteline = currentline;
-            // string to_remove = tname + ".txt";
-            // cout << remove(tname.c_str()) << endl;
-            // cout<<remove(tname.c_str());
         }
     }
     table.close();
-    if (tablepresent == false)
+
+    return tablepresent;
+}
+
+
+void insert(vector<string> &querytocons){
+    string schema = "schema.txt";
+    string tname = querytocons[2];
+    string requiredline;
+    if (querytocons.size() < 2){
+        cout << "Incorrect Query" << endl;
+        return;
+    }
+
+    if (!isTablePresent(tname, requiredline))
     {
         cout << "Table does not exists.." << endl;
     }
-    else
-    {
 
+    if (querytocons[3] == "values"){
+        if (querytocons[4] == "("){
+            
+        }
+    }
+}
+
+
+void describeTable(vector<string> &querytocons){
+    string tname = querytocons[1];
+    string requiredline = "";
+    if (!isTablePresent(tname, requiredline)){
+        cout << "Table does not exists.." << endl;
+        return;
+    }
+
+    int i = tname.size() + 1;
+    int countHash = 0;
+
+    cout << endl << endl;
+    cout << "------" << "Table " << tname << "-----" << endl;
+    while(i < requiredline.size()){
+        if (requiredline[i] == '#'){
+            if (countHash % 2 == 0){
+                cout << "--";
+            }
+            else{
+                cout << endl;
+            }
+            countHash++;
+        }
+        else{
+            cout << requiredline[i];
+        }
+        i++;
+    }
+
+    return;
+}
+
+
+void dropTable(vector<string> &querytocons)
+{
+    string schema = "schema.txt";
+    string tname = querytocons[2];
+    string deleteline;
+    if (querytocons.size() < 2){
+        cout << "Incorrect Query" << endl;
+        return;
+    }
+
+    if (!isTablePresent(tname, deleteline)){
+        cout << "Table does not exists.." << endl;
+        return;
+    }
+    else{
         fstream table;
         ofstream fout;
         fout.open("tempfile.txt", ios::out);
@@ -50,16 +106,7 @@ void droptable(vector<string> &querytocons)
             string line;
             getline(table, line);
             cout.flush();
-            cout << line[0] << " ";
-            // if (line.substr(0, tname.length()) == tname)
-            // {
-            //     exists = true;
-            // }
-            // else
-            // {
-            //     fout.write(line.data(), line.size());
-            //     fout << endl;
-            // }
+            //cout << line[0] << " ";
 
             if (line != deleteline && line != ""){
                 fout.write(line.data(), line.size());
@@ -68,13 +115,9 @@ void droptable(vector<string> &querytocons)
         }
         table.close();
         fout.close();
-        // char char_array[schema.size() + 1];
-
-        // strcpy(char_array, schema.c_str());
-        //  int res = remove(char_array);
         int res = remove("schema.txt");
         cout << res << endl;
-        //   string first = "tempfile1.txt";
+
         string toRemove = tname + ".txt";
         char char_array[toRemove.size() + 1];
         strcpy(char_array, toRemove.c_str());
@@ -85,9 +128,8 @@ void droptable(vector<string> &querytocons)
     }
 }
 
-void create(vector<string> &querytocons)
-{
 
+void create(vector<string> &querytocons){
     ofstream fout, foutfortable;
 
     string schema = "schema.txt";
@@ -103,18 +145,15 @@ void create(vector<string> &querytocons)
         getline(table, currentline);
         cout.flush();
 
-        if (currentline.substr(0, table_name.length()) == table_name)
-        {
+        if (currentline.substr(0, table_name.length()) == table_name){
             tablepresent = true;
         }
     }
 
-    if (tablepresent == true)
-    {
+    if (tablepresent == true){
         cout << "Table already exists!" << endl;
     }
-    else
-    {
+    else{
         vector<string> query;
         query = querytocons;
 
@@ -125,23 +164,41 @@ void create(vector<string> &querytocons)
         foutfortable.open(table_name, ios_base::app);
 
         fout << querytocons[2];
-        for (int j = 5; j < querytocons.size(); j += 3)
-        {
-            if (querytocons[j] == "int")
-            {
+        int j;
+        for (j = 5; j < querytocons.size(); j += 3){
+            // cout << querytocons[j] << " ";
+            if (querytocons[j].substr(0, 4) == "char"){
                 fout << "#" << querytocons[j - 1] << "#" << querytocons[j];
             }
-
-            if (querytocons[j] == "decimal")
-            {
+            else {
                 fout << "#" << querytocons[j - 1] << "#" << querytocons[j];
             }
-
-            if (querytocons[j].substr(0, 4) == "char")
-            {
-                fout << "#" << querytocons[j - 1] << "#" << querytocons[j];
-            }
+            // if (querytocons[j] == "primary"){
+            //     break;
+            // }
         }
+
+        // if (j != querytocons.size()-1){
+        //     if (querytocons[j] == "primary" && querytocons[j+1] == "key"){
+        //         j += 2; //to get in the brackets
+        //         while(j < querytocons.size()){
+        //             fout << "#" << querytocons[j] << "#pkey";
+        //             j += 2;
+
+        //             if (querytocons[j] == ","){
+        //                 j++;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // if (j != querytocons.size()-1){
+        //     if (querytocons[j] == "foreigh" && querytocons[j+1] == "key"){
+                
+        //     }
+        // }
+
         fout << endl;
 
         fout.close();
@@ -159,57 +216,55 @@ int main()
     stringstream ss(s);
     string temp;
     vector<string> querytocons;
-    while (ss >> temp)
-    {
+    while (ss >> temp){
         transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-        //cout << temp << endl;
         querytocons.push_back(temp);
     }
 
-    if (querytocons[0] == "create")
-    {
-        if (querytocons[1] == "table")
-        {
+    if (querytocons[0] == "create"){
+        if (querytocons[1] == "table"){
 
-            if (querytocons[3] == "(" and querytocons[querytocons.size() - 1] == ";" and querytocons[querytocons.size() - 2] == ")")
-            {
+            if (querytocons[3] == "(" and querytocons[querytocons.size() - 1] == ";" and querytocons[querytocons.size() - 2] == ")"){
                 bool corr = true;
-                for (int i = 6; i < querytocons.size() - 2; i += 3)
-                {
-                    if (querytocons[i] != ",")
-                    {
+                for (int i = 6; i < querytocons.size() - 2; i += 3){
+                    if (querytocons[i] != ","){
                         corr = false;
                         break;
                     }
                 }
-                if (corr)
-                {
+                if (corr){
                     create(querytocons);
                 }
 
-                else
-                {
+                else{
                     cout << "Incorrect Query" << endl;
                 }
             }
-            else
-            {
+            else{
                 if (querytocons[querytocons.size() - 1] != ";")
                     cout << "Please provide ; at the end" << endl;
                 else
                     cout << "Incorrect Query!!" << endl;
             }
         }
-        else
-        {
+        else{
             cout << "Incorrect syntax!!!" << endl;
         }
     }
-    else if (querytocons[0] == "drop")
-    {
-        if (querytocons[1] == "table")
-        {
-            droptable(querytocons);
+    else if (querytocons[0] == "drop"){
+        if (querytocons[1] == "table"){
+            dropTable(querytocons);
         }
     }
+    else if (querytocons[0] == "describe"){
+        describeTable(querytocons);
+    }
+    else if (querytocons[0] == "insert"){
+        if (querytocons[1] == "into"){
+            insert(querytocons);
+        }
+    }
+
+
+    return 0;
 }
